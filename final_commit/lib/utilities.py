1ddcab3f0ca4e8509d5d1b82c5a80c2ba4b96a82
Valerio-Amico <valerio.amico98@gmail.com>
2022-04-12 19:08:40 +0200

    added some functions in utilities

--- final_commit/lib/utilities.py
+++ final_commit/lib/utilities.py
@@ -1,13 +1,16 @@
+from logging import warning
 import numpy as np
-from copy import deepcopy
+import copy
 from qiskit import (
     Aer,
     QuantumCircuit,
     QuantumRegister,
     ClassicalRegister,
-    execute
+    execute,
+    transpile
 )
-from qiskit.ignis.verification.tomography import StateTomographyFitter
+from qiskit.utils.mitigation.fitters import CompleteMeasFitter
+from qiskit.ignis.verification.tomography import StateTomographyFitter, state_tomography_circuits
 from qiskit.quantum_info import state_fidelity
 from scipy.linalg import expm
 import os
@@ -172,6 +175,139 @@ def fidelity_count(result, qcs, target_state):
     return fid
 
 
+
+
+
+def get_evolution_circuit(time, n_steps, method="HSD"):
+    if method == "HSD":
+        return get_HSD_circuit(time, n_steps)
+    if method == "SSD":
+        return get_SSD_circuit(time, n_steps)
+    return "specify a decomposition technique (HSD or SSD)"
+
+def get_HSD_circuit(time, n_steps, initial_state={"110": 1}):
+    # Build the permutation operator
+    B_qr=QuantumRegister(3, name="q_")
+    B_qc=QuantumCircuit(B_qr, name="B")
+    B_qc.x(B_qr[2])
+    B_qc.cx(B_qr[1],B_qr[0])
+    B_qc.cx(B_qr[2],B_qr[1])
+    B_qc.cx(B_qr[1],B_qr[0])
+    B_qc.x([B_qr[0],B_qr[1],B_qr[2]])
+    B_qc.append(Toffoli_gate, [B_qr[0],B_qr[1],B_qr[2]])
+    B_qc.x([B_qr[0],B_qr[1]])
+
+    B = np.array(
+       [[0,0,0,0,1,0,0,0],
+        [0,0,1,0,0,0,0,0],
+        [0,1,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,1,0,0],
+        [0,0,0,0,0,0,1,0],
+        [0,0,0,0,0,0,0,1]]
+    )
+
+    T = trotterized_matrix(time, n_steps)
+    T_b = np.linalg.multi_dot([ B, T, B.transpose() ])
+
+    D = T_b[0:4, 0:4]
+
+    from qiskit import transpile
+    # Transpile the D operator and build the evolution circuit
+    D_qc = QuantumCircuit(2, name="D")
+    D_qc.unitary(D,[0,1])    
+    D_qc = transpile(D_qc, basis_gates=['cx','x','sx','rz']) # Jackarta basis gates
+
+    qr_HSD = QuantumRegister(3, name="q_")
+    qc_HSD = QuantumCircuit(qr_HSD, name="D")
+
+    # the permutation of 110 is 110 itself, so we prepare it.
+    initial_state = "110"
+
+    for qubit in range(3):
+        if initial_state[::-1][qubit] == "1":
+            qc_HSD.x(qr_HSD[qubit])
+
+    qc_HSD.append(D_qc, [qr_HSD[0], qr_HSD[1]])
+    qc_HSD.append(B_qc.inverse(), qr_HSD)
+
+    return qc_HSD
+
+def get_SSD_circuit(time, n_steps, initial_state={"110": 1}):
+    # getting the parameters for the gates M1 and M2, solving the equations described in 1.1).
+    theta_1, theta_2, phi_1, phi_2, omega_1, omega_2 = get_gates_parameters(trotterized_matrix(time, n_steps), initial_state=initial_state)
+    # build M1 and M2
+    M1_qc = _get_M(theta_1, phi_1, omega_1)
+    M2_qc = _get_M(theta_2, phi_2, omega_2)
+    # define the circuit of U
+    qr_U = QuantumRegister(3 ,name="q_")
+    qc_U = QuantumCircuit(qr_U, name="U")
+    # append the gates
+    qc_U.append(M1_qc, [qr_U[0], qr_U[1]])
+    qc_U.append(M2_qc, [qr_U[1], qr_U[2]])
+    # transpile and draw the circuit
+    from qiskit import transpile
+    qc_U=transpile(qc_U, basis_gates=["cx","rz","x","sx"])
+    return qc_U
+
+def fast_tomography_calibration_MeasFitters(qc, results, method="NIC"):
+
+    state_labels = ['000', '001', '010', '011', '100', '101', '110', '111']  
+    meas_fitter = CompleteMeasFitter(results, state_labels=state_labels)
+
+    U_tilde = meas_fitter.cal_matrix
+
+    #defining the tomography basis circuits.
+    qr_basi = QuantumRegister(3)
+    qc_basi = QuantumCircuit(qr_basi)
+    tomography_basis = state_tomography_circuits(qc_basi, qr_basi)
+    # computing the ideal matrix of the circuit.
+    U_ideal = matrix_from_circuit(qc)
+    U_ideal_abs = np.abs(U_ideal)**2
+    U_ideal_abs_inv = np.linalg.inv(U_ideal_abs)
+
+    if method == "NIC":
+        C = U_tilde
+    elif method == "CIC":
+        C = np.dot(U_tilde, U_ideal_abs_inv)
+    # building the fast tomography circuits calibration.
+    C_matrices = []
+    for basis in tomography_basis:
+        # compute the tomography unitary basis matrix and the inverse
+        basis.remove_final_measurements()
+        base_matrix_amplitudes = matrix_from_circuit(basis)
+        base_matrix_amplitudes_inverse = np.linalg.inv(base_matrix_amplitudes)
+        # compute the probability matrix of the base changing
+        base_matrix = np.abs(base_matrix_amplitudes)**2
+        base_matrix_inverse = np.abs(base_matrix_amplitudes_inverse)**2
+        # compute the calibration matrix in the new basis
+        C_base = np.linalg.multi_dot([base_matrix, C,  base_matrix_inverse])
+        C_matrices.append(C_base)
+        
+    meas_fitters = []
+    for tomography_basis in range(3**3):
+        meas_fitter_aus = copy.deepcopy(meas_fitter)
+        meas_fitter_aus._tens_fitt.cal_matrices[0]=C_matrices[tomography_basis]
+        meas_fitters.append(meas_fitter_aus)
+
+    return meas_fitters
+
+
+def _get_M(theta, phi, omega, name="M"): # defining the M matrix
+
+    qr=QuantumRegister(2, name="q_")
+    M_qc=QuantumCircuit(qr, name=name)
+
+    M_qc.rz(2*theta,qr[1])
+    M_qc.h(qr[0])
+    M_qc.cx(qr[0],qr[1])
+    M_qc.ry(omega,qr)
+    M_qc.cx(qr[0],qr[1])
+    M_qc.h(qr[0])
+    M_qc.rz(2*phi,qr[1])
+
+    return M_qc
 
 
 def DecimalToBinary(num, number_of_qubits):
